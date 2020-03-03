b666fb046881585bec19b505c165ed3953f1a244
Jetpuffed <brandon@jetpuffed.org>
2020-03-03 15:10:49 -0800

    tweaked settings for deployment

--- Tutorial/tutorial/settings.py
+++ Tutorial/tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!24cpq5wyzm9m59__n@9lyfi^^=^&!#=&!)$cv7qb*o$)qvs+*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'uniqueprojectname.herokuapp.com']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
