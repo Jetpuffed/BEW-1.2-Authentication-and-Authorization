1116676f563ec5ef0b5f662cf26a54f60097c55a
Jetpuffed <brandon@jetpuffed.org>
2020-03-03 15:46:12 -0800

    heroku stuff

--- makewiki/makewiki/settings.py
+++ makewiki/makewiki/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1yct-t!2bnkgc7j59z+9cdd2k)@y+ftqor$!aya()3if^cnlo-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'brandon-bew1-2-makewiki.herokuapp.com']
 
 
 # Application definition
@@ -126,7 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
 
 # wiki app settings
 WIKI_PAGE_TITLE_MAX_LENGTH = 600
